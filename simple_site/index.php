892501330bb49f6a44a3f2ba0044a2241b8b01dd
Roman Surkov <surkov.r.b@gmail.com>
2025-03-30 12:56:35 +0200

    simple site project

---
+++ simple_site/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Личный сайт Брони</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body >
+
+<div class="content">
+<div class="header">
+	<a href="#">Главная</a>
+	<a href="puzzle.html">Загадки</a>
+	<a href="guess.html">Угадайка</a>
+</div>
+
+<h1>Личный сайт Брони</h1>
+
+<div class="center">
+<img src="img/сн.png"/>
+		<div class="box_text">
+
+<p><i>Меня зовут Броня я недавно начал програмировать 
+и это мой первый сайт.</i></p>
+
+
+<p> В этом мне помог IT-портал 
+	<a href="https://geekbrains.ru">GeekBrains</a>
+</p>
+
+<p>На этом сайте вы сможите сыграть в
+несколько игр, которые я написал:
+	<a href="#">Главная</a>,
+	<a href="#">Загадки</a>,
+	<a href="#">Угадайка</a>
+</p> 
+<div class="footer">
+    Copyright &copy; <?php echo date("Y");?> Bronislav
+<div>
+
+
+</body>
+</html>
